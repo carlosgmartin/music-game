028ea1eac2581c0e634a0c627dfe5522d657ead9
Carlos Martin <carlos.martin@columbia.edu>
2016-02-04 23:45:16 -0500

    Very simple beat detection using spectral flux

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ var decodeSuccessCallback = function(audioBuffer) {
 	audioBufferSourceNode = audioContext.createBufferSource();
 	audioBufferSourceNode.buffer = audioBuffer;
 	audioBufferSourceNode.connect(audioContext.destination);
-	audioBufferSourceNode.start();
 
 	processAudioBuffer(audioBuffer);
 };
@@ -41,16 +40,38 @@ var processAudioBuffer = function(audioBuffer) {
 	// Get PCM data (samples)
 	var data = audioBuffer.getChannelData(0);
 	var fft = new FFT(frameSize, audioBuffer.sampleRate);
+
+	var frames = Math.floor((data.length - frameSize) / frameSize);
+	var spectralFlux = new Float32Array(frames);
+	var lastSpectrum = new Float32Array(frameSize);
+	var frame = 0;
+
 	for (var sample = 0; sample < data.length - frameSize; sample += frameSize) {
 		// Get samples belonging to current frame
 		var frameData = data.slice(sample, sample + frameSize);
 		// Get current time in seconds
 		var seconds = sample / audioBuffer.sampleRate;
-
+		// Calculate current frame FFT with windowing
     	fft.forward(frameData);
-		console.log('Time ' + seconds + ': ' + fft.spectrum[0]);
+		var currentSpectrum = fft.spectrum;
+
+		for (var freq = 0; freq < currentSpectrum.length; ++freq) {
+			var difference = currentSpectrum[freq] - lastSpectrum[freq];
+			if (difference > 0) {
+				spectralFlux[frame] += difference;
+			}
+		}
+		lastSpectrum = new Float32Array(currentSpectrum);
+		++frame;
 	}
-	console.log(audioBuffer.duration);
+	for (var frame = 0; frame < spectralFlux.length; ++frame) {
+		if (spectralFlux[frame] > 2.3) {
+			var seconds = frame * frameSize / audioBuffer.sampleRate;
+			// console.log('Time ' + seconds + ': ' + spectralFlux[frame]);
+			setTimeout(function() { console.log('beat'); }, seconds * 1000);
+		}
+	}
+	audioBufferSourceNode.start();
 };
 
 
